54ee26919885cb0219d1d571fa75f41b41276f06
MainUddin1223 <2021md.main@gmail.com>
2022-06-02 01:52:25 +0600

    initial design done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF, faInstagram, faLinkedinIn, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
@@ -8,18 +8,28 @@ const Footer = () => {
         <div className='footer'>
             <div className="project-body">
                 <div className='footer-social'>
+
                     <div className='top-banner-social'>
-                        <FontAwesomeIcon icon={faPhone} />
-                        <p className='contact-info'>+1212121212 /</p>
-                        <FontAwesomeIcon icon={faWhatsapp} />
-                        <p className='contact-info'>+5465555545 <span className='divider'>|</span></p>
-                        <FontAwesomeIcon icon={faEnvelope} />
-                        <p>Email: <span className='contact-info'>demo@demo.com</span></p>
+                        <FontAwesomeIcon icon={faEnvelope} className="social-icon-footer" />
+                        <p>Email: demo.demo@gmail.com</p>
+                    </div>
+                    <div className='top-banner-social'>
+                        <FontAwesomeIcon icon={faPhone} className="social-icon-footer" />
+                        <p className=''>+1212121212 /</p>
+                        <FontAwesomeIcon icon={faWhatsapp} className="social-icon-footer" />
+                        <p className=''>+5465555545</p>
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={faFacebookF} className="social-icon-footer" />
+                        <FontAwesomeIcon icon={faInstagram} className="social-icon-footer" />
+                        <FontAwesomeIcon icon={faLinkedinIn} className="social-icon-footer" />
                     </div>
                 </div>
-                <div>
-                    <p>Copyright {new Date().getFullYear()}</p>
-                </div>
+            </div>
+            <hr />
+
+            <div className='copy-right'>
+                <p>Copyright {new Date().getFullYear()}</p>
             </div>
         </div>
     );
